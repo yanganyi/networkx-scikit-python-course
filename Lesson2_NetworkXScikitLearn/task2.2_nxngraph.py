b81a1bceef5cc24e9f5a0331bd996bcb914bbfbc
An YI <61081228+yanganyi@users.noreply.github.com>
2022-09-07 09:48:38 +0800

    [Bug] Bug Fix
    bug in task 2.2
    boundary checks now dependent on N

--- Lesson2_NetworkXScikitLearn/task2.2_nxngraph.py
+++ Lesson2_NetworkXScikitLearn/task2.2_nxngraph.py
@@ -10,19 +10,16 @@ l=[]
 nodes=int(input("N: "))
 for i in range(nodes):
     l.append(i)
-    
 for i in range(nodes**2):
     G.add_node(i)
-    if i<20: G.add_edge(i,i+5)
-    if i%5!=4 and i<24: G.add_edge(i,i+1)
-    
+    # checks before adding edges to not go out of bounds
+    if i<(nodes*(nodes-1)): G.add_edge(i,i+nodes)
+    if i%nodes!=(nodes-1) and i<(nodes**2-1): G.add_edge(i,i+1)
 pos = {i: (1,1) for i in enumerate(l)}
 pos.update({0: (0,0)})
 for i in range(nodes):
     for j in range(nodes):
         pos.update({nodes*i+j: (0.5*j,0.5*i)})
-
-
 
 nx.draw(G,pos,with_labels=True,font_weight="bold")
 # pos=nx.spiral_layout(G)
